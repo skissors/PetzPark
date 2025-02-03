e0a2f8943f9b27dbbc7723d26051a113f94decf0
lilliancf <run.with.skissors@gmail.com>
2025-02-02 20:52:47 -0600

    added ralph

--- data/parkPetz.js
+++ data/parkPetz.js
@@ -1,14 +1,20 @@
 petz = [
+	{
+		name: "Jack",
+		owner: "skissors",
+		site: "Alive Enough",
+		url: "https://oodlecat.meowandsparkle.party/",
+	},
+	{
+		name: "Fitz",
+		owner: "skissors",
+		site: "Alive Enough",
+		url: "https://oodlecat.meowandsparkle.party/",
+	},
     {
-        "name":"Fitz",
-        "owner":"skissors",
-        "site":"Alive Enough",
-        "url":"https://oodlecat.meowandsparkle.party/"
-    },
-    {
-        "name":"Jack",
-        "owner":"skissors",
-        "site":"Alive Enough",
-        "url":"https://oodlecat.meowandsparkle.party/"
-    },    
-]
+		name: "Ralph",
+		owner: "ECRose",
+		site: "Mythic Silence",
+		url: "https://www.mythicsilence.com/malevolent/index.php",
+	},
+];
